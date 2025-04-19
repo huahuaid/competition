a069e16bbbf5f2774bf5ed8421f3e39dec8290ca
wyhlion <imwyh159@qq.com>
2025-04-19 22:30:38 +0800

    Zero_level里的题目面板是为了检查功能，实际使用时激活Perfab文件里的同名预物体就行,但是代码写得并不智能，不能特定使用哪一个选择题，后续再优化吧

--- Assets/Scripts/ClickOptions/ClickUITextDetector.cs
+++ Assets/Scripts/ClickOptions/ClickUITextDetector.cs
@@ -45,24 +45,5 @@ public class ClickUITextDetector : MonoBehaviour, IPointerClickHandler
 			}
 		}
 	}
-	//
-	// private string[] questions = { "QuestionOne", "QuestionTwo", "QuestionThree", "QuestionFour", "QuestionFive" };
-	//
-	// private string[] options = {"A" , "B" , "A" , "C" , "B"};
-	//
-	// public bool Judege(string option , string questionName)
-	// {
-	// 	int count = 0;
-	//
-	// 	for (int i = 0; i < questions.Length; i++)
-	// 	{
-	// 		if (questions[i] == questionName)
-	// 		{
-	// 			count = i;
-	// 			break;
-	// 		}
-	// 	}
-	//
-	// 	return options[count] == option.Split(' ')[0];
-	// }
+	
 }
